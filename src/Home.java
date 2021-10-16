2cc5245c7d0e26a7a700bf7fc3f1b8a028050d98
bhupesh <bhupesh198817@gmail.com>
2021-10-16 17:48:10 +0100

    commit message

---
+++ src/Home.java
@@ -0,0 +1,14 @@
+public class Home {
+
+    public void m1(int x, int y) {
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+    }
+
+    public static void main(String[] args) {
+        Home home=new Home();
+        home.m1(23, 22);
+    }
+    }
